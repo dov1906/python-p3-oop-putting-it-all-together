f15ba7b8af33a840b51b032fb99f8a54e752fa37
Dov Azogui <dov.azogui@gmail.com>
2024-10-07 18:47:28 -0400

    coded

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count  
+        
     
+    @property
+    def page_count(self):
+        return self.page
+
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) == int:
+            self.page = value
+        else:
+            print("page_count must be an integer")
+            
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    
+book1 = Book("hello", 100)
+
+
+
+
+    
+
         
